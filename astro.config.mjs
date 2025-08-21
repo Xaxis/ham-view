2b3b37c90022b8c8ad64bccd05676b43df0e82d8
Xaxis <william.neeley@gmail.com>
2025-08-20 18:22:30 -0800

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,20 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'static',
-  site: 'https://hamview.com'
+    integrations: [react()],
+    output: 'static',
+    site: 'https://hamview.com',
+    base: '/',
+    build: {
+        assets: 'assets'
+    },
+    vite: {
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: undefined
+                }
+            }
+        }
+    }
 });
